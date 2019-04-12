0dd071945ff86fae7c143160f32eed3e2b15882a
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-12 10:58:41 -0600

    Adds validations to Review model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :rating }
   end
 
   describe 'relationships' do
     it { should belong_to :user }
-    it { should belong_to :order_item}
+    it { should belong_to :order_item }
   end
 
 end
