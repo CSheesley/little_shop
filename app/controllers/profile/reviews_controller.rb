cf4a8e3f787c86903aac7a32d4d5932c10be9e13
Corey Sheesley <corey.sheesley@gmail.com>
2019-04-12 13:28:10 -0600

    User able to edit a review

--- app/controllers/profile/reviews_controller.rb
+++ app/controllers/profile/reviews_controller.rb
@@ -3,4 +3,35 @@ class Profile::ReviewsController < ApplicationController
   def index
     @reviews = Review.where(user: current_user.id)
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    @review = current_review
+  end
+
+  def update
+    @review = current_review
+    if @review.update(update_params)
+      flash[:update] = "Review for #{@review.title} has been updated!"
+      redirect_to profile_reviews_path
+    else
+      flash[:errors] = @review.erros.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  private
+
+  def current_review
+    Review.find(params[:id])
+  end
+
+  def update_params
+    params.require(:review).permit(:title, :rating, :description)
+  end
 end
--- spec/features/users/reviews/index_spec.rb
+++ spec/features/users/reviews/index_spec.rb
@@ -19,18 +19,20 @@ RSpec.describe 'Profiles Reviews Index Page', type: :feature do
 
       within "#my-reviews-#{@review_1.id}" do
         expect(page).to have_content("Title: #{@review_1.title}")
+        expect(page).to have_content("Rating: #{@review_1.rating}")
         expect(page).to have_content("Description: #{@review_1.description}")
         expect(page).to have_content("Created/Updated: #{@review_1.updated_at.strftime("%m/%d/%y")}")
       end
 
       within "#my-reviews-#{@review_2.id}" do
         expect(page).to have_content("Title: #{@review_2.title}")
+        expect(page).to have_content("Rating: #{@review_2.rating}")
         expect(page).to have_content("Description: #{@review_2.description}")
         expect(page).to have_content("Created/Updated: #{@review_2.updated_at.strftime("%m/%d/%y")}")
       end
     end
 
-    xit 'gives me an option to edit or delete any of my reviews' do
+    it 'gives me an option to edit or delete any of my reviews' do
 
       within "#my-reviews-#{@review_1.id}" do
         expect(page).to have_link("Edit")
@@ -43,15 +45,40 @@ RSpec.describe 'Profiles Reviews Index Page', type: :feature do
       end
     end
 
-    xit 'does not give me the ability to add a new review anywhere on this page' do
+    it 'does not give me the ability to add a new review anywhere on this page' do
 
       expect(page).to_not have_content("Add New Review")
       expect(page).to_not have_content("New Review")
     end
 
     context 'when I click on the Edit button next to a review' do
-      xit 'I see a pre-populated form - after I click submit, I receive a notification and see the changes on my reviews page' do
+      it 'I see a pre-populated form - after I click submit, I receive a notification and see the changes on my reviews page' do
 
+        within "#my-reviews-#{@review_1.id}" do
+          click_on "Edit"
+          expect(current_path).to eq(edit_profile_review_path(@review_1))
+        end
+
+        expect(find_field("Title").value).to eq("#{@review_1.title}")
+        expect(find_field("Rating").value).to eq("#{@review_1.rating}")
+        expect(find_field("Description").value).to eq("#{@review_1.description}")
+
+        fill_in "Title", with: "Item Broke"
+        fill_in "Rating", with: 1
+        fill_in "Description", with: "I changed my mind, this item broke"
+
+        click_on "Update Review"
+
+        @review_1.reload
+
+        expect(current_path).to eq(profile_reviews_path)
+        expect(page).to have_content("Review for #{@review_1.title} has been updated!")
+
+        within "#my-reviews-#{@review_1.id}" do
+          expect(page).to have_content("Title: Item Broke")
+          expect(page).to have_content("Rating: 1")
+          expect(page).to have_content("Description: I changed my mind, this item broke")
+        end
       end
     end
 
